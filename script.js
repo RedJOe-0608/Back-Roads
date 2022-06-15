bbf2b3e20db5b1b32c22861366aceaefb5d29ac5
Jyothi Swaroop Makena <joeswaroopmakena@gmail.com>
2022-06-15 23:11:34 +0530

    NavLinks modified

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const navLinks = document.querySelector('.nav-links');
 const navToggle = document.querySelector('.nav-toggle');
+const navLink = document.querySelectorAll('.nav-link');
 
 navToggle.addEventListener('click', () =>{
     console.log("clicked");
     navLinks.classList.toggle('show');
+})
+
+// navLink.addEventListener('click',() =>{
+//     console.log('clicked');
+//     navLinks.classList.toggle('show');
+// })
+
+navLink.forEach((link) =>{
+    link.addEventListener('click',() =>{
+    console.log('clicked');
+    navLinks.classList.toggle('show');
+})
 })
